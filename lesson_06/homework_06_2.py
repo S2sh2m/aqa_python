5cca86e4481fbeee88119761073b5dd388852b21
S2sh2m <70859559+S2sh2m@users.noreply.github.com>
2025-08-19 20:37:56 +0200

    Виконані зміни

---
+++ lesson_06/homework_06_2.py
@@ -0,0 +1,9 @@
+
+while True:
+    symbols = input("You have to add here any 'h' or 'H'")
+
+    if "h" in symbols or "H" in symbols:
+        print("Good job: ")
+        break
+    else:
+        print("One more try: ")
